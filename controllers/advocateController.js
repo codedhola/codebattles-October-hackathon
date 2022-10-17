8ec98e3281b498978a712016285752a8f6fcca1a
codedhola <codedhola2000@gmail.com>
2022-10-18 00:54:57 +0100

    FEAT: Added query feature

--- controllers/advocateController.js
+++ controllers/advocateController.js
@@ -3,14 +3,33 @@ const Advocate = require("./../models/advocateModel");
 const getAllAdvocates = async (req, res) => {
 // SEARCH ALL QUERY PARAMATERS EXCEPT EXCLUDES
     const queryObj = {...req.query};
-    let excludes = ["sort", "page", "limit"];
+    let excludes = ["sort", "page", "limit", "name"];
     excludes.forEach((el) => delete queryObj[el]);
-    const name = req.query.name;
     
     try{
         // REGEX TO SEARCH A NAME  => {name: {$regex: new RegExp(`${name}`, 'g')}}
-        let query = Advocate.find({});
-        query = query
+        let query = Advocate.find(queryObj);
+
+        if(req.query.name){
+            query = query.find({name: {$regex: new RegExp(`${req.query.name}`, 'g')}});
+        }
+        if(req.query.sort){
+            
+        }
+
+        // LIMITING FIELDS
+        if(req.query.fields){
+            let fields = req.query.fields.split(",").join(" ")
+            query = query.select(fields);
+        }else {
+            query = query.select("-__v")
+        }
+        
+        let page = req.query.page || 1;
+        let limit = req.query.limit || 2;
+        let skip = (page - 1) * limit;
+
+        query = query.skip(skip).limit(limit);
         
         const data = await query;
         res.status(200).json({
